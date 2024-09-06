968d658532c75b249bbcc0bf282a3e4fae38f931
DNNThien <dnnthien2903@gmail.com>
2024-09-06 15:50:19 +0700

    Include 2 folder is search and sort

---
+++ search/search.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int songaunhien(int a = 1, int b = 100)
+{
+    return a + rand() % (b - a + 1);
+}
+
+void nhapmang(int *a, int n, int x = 1, int y = 100)
+{
+    for(int i=0;i<n;i++)
+        *(a+i) = songaunhien(x,y);
+}
+
+void xuatmang(int *a, int n, int option = 1)
+{
+    if(option)
+        for(int i=0;i<n;i++)
+            printf("%d ",*(a+i));
+    else
+        for(int i=n-1;i>=0;i--)
+            printf("%d ",*(a+i));
+    printf("\n");
+}
+
+int sequentialSearch(int *a, int n, int value)
+{
+    if(n<=0) return -1;
+    for(int i=0;i<n;i++)
+        if(*(a+i) == value)
+            return i;
+    return -1;
+}
+
+int main()
+{
+    srand(time(NULL));
+    int n = songaunhien(5,20);
+    printf("So luong phan tu: %d\n",n);
+    int *a = (int*)malloc(n * sizeof(int));
+    nhapmang(a,n);
+    printf("Mang vua nhap: ");
+    xuatmang(a,n);
+    int value = *(a+songaunhien(0,n-1));
+    printf("Gia tri %d nam o vi tri %d", value, sequentialSearch(a,n,value));
+    free(a);
+}
